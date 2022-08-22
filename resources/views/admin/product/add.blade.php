934ed3a9e1db9ef2d0b0a86bf8633f6349f13aea
Aidil <aidil03f@gmail.com>
2022-08-22 09:42:11 +0800

    upload image multiple product list

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -91,62 +91,24 @@
                     </div>
                   </div>
                   <div class="row">
-                    <div class="col-md-4">
+                     <div class="col-md-12">
                         <div class="form-group">
-                           <label for="image1">Image 1</label><br>
-                           <input type="file" accept="image/*" id="image1" name="image[]">
+                           <label for="image">Image</label>
+                           <div class="input-group mb-3">
+                              <div class="input-group-prepend">
+                                 <span class="input-group-text">Upload Image</span>
+                              </div>
+                              <div class="custom-file">
+                                 <input type="file" accept="image/*" onchange="previewImage()" class="custom-file-input" id="image" name="image[]" multiple>
+                                 <label class="custom-file-label">Choose File</label>
+                              </div>
+                           </div>
+                           <div class="form-group text-center my-2" id="imgpreview">
+
+                           </div>
                         </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer1" alt=""/>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                           <label for="image2">Image 2</label><br>
-                           <input type="file" accept="image/*" id="image2" name="image[]">
-                        </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer2" alt=""/>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                           <label for="image3">Image 3</label><br>
-                           <input type="file" accept="image/*" id="image3" name="image[]">
-                        </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer3" alt=""/>
-                        </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-4">
-                        <div class="form-group">
-                           <label for="image4">Image 4</label><br>
-                           <input type="file" accept="image/*" id="image4" name="image[]">
-                        </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer4" alt=""/>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                           <label for="image5">Image 5</label><br>
-                           <input type="file" accept="image/*" id="image5" name="image[]">
-                        </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer5" alt=""/>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                           <label for="image6">Image 6</label><br>
-                           <input type="file" accept="image/*" id="image6" name="image[]">
-                        </div>
-                        <div class="form-group text-center" style="margin-bottom:0%;">
-                           <img style="width: 50%;border: 0px solid; border-radius: 10px;" id="viewer6" alt=""/>
-                        </div>
-                    </div>
+                     </div>
+
                   </div>
                   <div class="text-right mt-2">
                      <a href="{{ route('admin.product.index') }}" class="mb-2 mr-2 btn btn-icon btn-light btn-lg"><i class="pe-7s-back btn-icon-wrapper"></i>Back</a>
@@ -161,54 +123,19 @@
 @section('js')
     <script src="https://cdn.ckeditor.com/4.12.1/standard/ckeditor.js"></script>
     <script>
-       let content = document.getElementById('description')
-       CKEDITOR.replace(content, {
-           language: 'en-gb'
-       })
-       CKEDITOR.config.allowedContent = true;
-
-       function readURL(input) {
-            if (input.data.files && input.data.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                  if(input.image == 1){
-                     $('#viewer1').attr('src', e.target.result);
-                  }else if(input.image == 2){
-                     $('#viewer2').attr('src', e.target.result);
-                  }else if(input.image == 3){
-                     $('#viewer3').attr('src', e.target.result);
-                  }else if(input.image == 4){
-                     $('#viewer4').attr('src', e.target.result);
-                  }else if(input.image == 5){
-                     $('#viewer5').attr('src', e.target.result);
-                  }else if(input.image == 6){
-                     $('#viewer6').attr('src', e.target.result);
-                  }
-                }
-                reader.readAsDataURL(input.data.files[0]);
+      let content = document.getElementById('description');
+      CKEDITOR.replace(content, {
+         language: 'en-gb'
+      })
+      CKEDITOR.config.allowedContent = true;
+      function previewImage() {
+            for(var i=0; i < $('#image').get(0).files.length; i++){
+               $('#imgpreview').append("<img class='img-thumbnail' style='object-fit: cover' height='250' width='250' src='"+URL.createObjectURL(event.target.files[i])+"' alt=''/>");
             }
-        }
-        $("#image1").change(function () {
-            readURL({image:1,data:this});
-        });
-        $("#image2").change(function () {
-            readURL({image:2,data:this});
-        });
-        $("#image3").change(function () {
-            readURL({image:3,data:this});
-        });
-        $("#image4").change(function () {
-            readURL({image:4,data:this});
-        });
-        $("#image5").change(function () {
-            readURL({image:5,data:this});
-        });
-        $("#image6").change(function () {
-            readURL({image:6,data:this});
-        });
-        $("#tags").select2({
-            tags: true,
-            tokenSeparators: [',', ' ']
-        });
+      }
+      $("#tags").select2({
+         tags: true,
+         tokenSeparators: [',', ' ']
+      });
     </script>
 @endsection
