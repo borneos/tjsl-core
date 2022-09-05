78b780f640a72725fbeb3c704abdc9e458b43bd2
aidil03f <aidil03f@gmail.com>
2022-09-05 16:09:46 +0800

    remove menu order from sidebar bc-403

--- resources/views/layouts/app-sidebar.blade.php
+++ resources/views/layouts/app-sidebar.blade.php
@@ -5,28 +5,6 @@
             <li>
               <a href="/dashboard" class="dashboard {{ count(request()->segments()) == 1 && request()->segments()[0] == 'dashboard' ? 'mm-active' : '' }}"><i class="metismenu-icon pe-7s-rocket"></i>Dashboards</a>
             </li>
-            <li class="app-sidebar__heading">Orders</li>
-            <li>
-              <a class="new" href="/admin/orders/new"><i class="metismenu-icon pe-7s-download"></i>New</a>
-            </li>
-            <li>
-              <a class="processing" href="/admin/orders/processing"><i class="metismenu-icon pe-7s-hourglass"></i>Processing</a>
-            </li>
-            <li>
-              <a class="otw" href="/admin/orders/otw"><i class="metismenu-icon pe-7s-bicycle"></i>Product OTW</a>
-            </li>
-            <li>
-              <a class="delivered" href="/admin/orders/delivered"><i class="metismenu-icon pe-7s-cup"></i>Delivered</a>
-            </li>
-            <li>
-              <a class="canceled" href="/admin/orders/canceled"><i class="metismenu-icon pe-7s-close"></i>Canceled</a>
-            </li>
-            <li>
-              <a class="done" href="/admin/orders/done"><i class="metismenu-icon pe-7s-like2"></i>Done</a>
-            </li>
-            <li>
-              <a class="all" href="/admin/orders"><i class="metismenu-icon pe-7s-news-paper"></i>All</a>
-            </li>
             <li class="app-sidebar__heading">Master Data</li>
         </ul>
         <ul class="vertical-nav-menu">
